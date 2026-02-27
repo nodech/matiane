ac7af5e58f9e3089159e6cb2cae7faa990502734
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-02-27 11:59:32 +0400

    feat(core/regex): compile to nfa.

---
+++ matiane-core/src/regex.rs
@@ -0,0 +1,30 @@
+use thiserror::Error;
+
+mod lexer;
+mod parser;
+
+pub struct Regex<'a> {
+    src: &'a str,
+    nfa: parser::Nfa,
+}
+
+#[derive(Debug, Error)]
+pub enum RegexCompileError {
+    #[error("Lexer failed: {0}")]
+    LexError(#[from] lexer::LexError),
+    #[error("Parse failed: {0}")]
+    ParseError(#[from] parser::ParseError),
+}
+
+impl<'a> Regex<'a> {
+    pub fn compile(raw_regex: &'a str) -> Result<Self, RegexCompileError> {
+        let tokens = lexer::tokenize(raw_regex.chars())?;
+        let postfix_tokens = lexer::topostfix(tokens)?;
+        let nfa = parser::NfaBuilder::build(&postfix_tokens)?;
+
+        Ok(Self {
+            src: raw_regex,
+            nfa,
+        })
+    }
+}
