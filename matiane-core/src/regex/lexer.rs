e78030c4e189c17821e9b133aee6fab4575cb656
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2026-02-27 11:54:57 +0400

    feat(regex): Add lexer and postfix.

---
+++ matiane-core/src/regex/lexer.rs
@@ -0,0 +1,309 @@
+use std::fmt::Debug;
+use thiserror::Error;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum LexError {
+    #[error("Unexpected EOF at {0}")]
+    UnexpectedEof(usize),
+    #[error("Unexpected Caret at {0}")]
+    UnexpectedCaret(usize),
+    #[error("Unexpected dollar at {0}")]
+    UnexpectedDollar(usize),
+    #[error("Unbalance parens")]
+    UnbalancedParens,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Token {
+    Char(char),
+    Dot,    // .
+    Star,   // *
+    Plus,   // +
+    Caret,  // ^
+    Dollar, // $
+    Pipe,   // |
+    LParen, // (
+    RParen, // )
+    // LBracket, // [
+    // RBracket, // ]
+    // LBrace,   // {
+    // RBrace,   // }
+    // Dash,     // -
+    // Digits,  // \d
+    // Word,    // \w
+    // Space,   // \s
+    Concat, // pseudo element?
+}
+
+type TokenStream = Vec<Token>;
+
+#[derive(Debug, PartialEq)]
+pub(super) struct PostfixTokens(pub Vec<Token>);
+
+impl PostfixTokens {
+    pub fn iter(&self) -> std::slice::Iter<'_, Token> {
+        self.0.iter()
+    }
+}
+
+pub(super) fn tokenize(
+    raw: impl IntoIterator<Item = char>,
+) -> Result<TokenStream, LexError> {
+    let mut result: TokenStream = vec![];
+    let mut idx = 0;
+    let mut iter = raw.into_iter().peekable();
+
+    while let Some(ch) = iter.next() {
+        idx += 1;
+        let token = match ch {
+            '^' => {
+                if idx != 1 {
+                    return Err(LexError::UnexpectedCaret(idx));
+                }
+
+                Token::Caret
+            }
+            '$' => {
+                if iter.peek().is_some() {
+                    return Err(LexError::UnexpectedDollar(idx));
+                }
+
+                Token::Dollar
+            }
+            '\\' => {
+                let escaped =
+                    *iter.peek().ok_or(LexError::UnexpectedEof(idx))?;
+
+                iter.next();
+                Token::Char(escaped)
+            }
+            '\n' | '\r' => {
+                continue;
+            }
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            '.' => Token::Dot,
+            '|' => Token::Pipe,
+            '*' => Token::Star,
+            '+' => Token::Plus,
+            _ => Token::Char(ch),
+        };
+
+        insert_maybe_concat(&mut result, token);
+    }
+
+    Ok(result)
+}
+
+fn insert_maybe_concat(tokens: &mut TokenStream, token: Token) {
+    if let Some(last) = tokens.last()
+        && insert_concat_after(*last)
+        && insert_concat_before(token)
+    {
+        tokens.push(Token::Concat);
+    }
+
+    tokens.push(token);
+}
+
+fn insert_concat_after(token: Token) -> bool {
+    matches!(
+        token,
+        Token::Char(_) | Token::RParen | Token::Dot | Token::Star | Token::Plus
+    )
+}
+
+fn insert_concat_before(token: Token) -> bool {
+    matches!(
+        token,
+        Token::Char(_) | Token::Dot | Token::LParen | Token::Dollar
+    )
+}
+
+fn precedence(token: Token) -> usize {
+    match token {
+        Token::Star | Token::Plus => 3,
+        Token::Concat => 2,
+        Token::Pipe => 1,
+        _ => 0,
+    }
+}
+
+// Shunting
+pub(super) fn topostfix(
+    tokens: TokenStream,
+) -> Result<PostfixTokens, LexError> {
+    let mut ops: Vec<Token> = vec![];
+    let mut out = vec![];
+
+    for tok in tokens {
+        if tok == Token::LParen {
+            ops.push(tok);
+            continue;
+        }
+
+        if tok == Token::RParen {
+            loop {
+                let last = ops.pop().ok_or(LexError::UnbalancedParens)?;
+
+                if last == Token::LParen {
+                    break;
+                }
+
+                out.push(last);
+            }
+            continue;
+        }
+
+        let tok_prec = precedence(tok);
+
+        if tok_prec == 0 {
+            out.push(tok);
+            continue;
+        }
+
+        loop {
+            let last_op = ops.last();
+
+            let last_op = match last_op {
+                Some(token) => *token,
+                None => {
+                    ops.push(tok);
+                    break;
+                }
+            };
+
+            if last_op == Token::LParen {
+                ops.push(tok);
+                break;
+            }
+
+            let last_prec = precedence(last_op);
+
+            if last_prec < tok_prec {
+                ops.push(tok);
+                break;
+            }
+
+            out.push(ops.pop().unwrap());
+        }
+    }
+
+    for op in ops.into_iter().rev() {
+        if op == Token::LParen {
+            return Err(LexError::UnbalancedParens);
+        }
+
+        out.push(op);
+    }
+
+    Ok(PostfixTokens(out))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Token::*;
+    use super::*;
+
+    #[test]
+    fn test_simple_literals() {
+        assert_eq!(
+            tokenize("abcd".chars()).unwrap(),
+            vec![
+                Char('a'),
+                Concat,
+                Char('b'),
+                Concat,
+                Char('c'),
+                Concat,
+                Char('d')
+            ]
+        );
+    }
+
+    #[test]
+    fn test_escape() {
+        assert_eq!(
+            tokenize("\\\\\\(".chars()).unwrap(),
+            vec![Char('\\'), Concat, Char('('),]
+        );
+
+        assert_eq!(tokenize("ab \\".chars()), Err(LexError::UnexpectedEof(4)))
+    }
+
+    #[test]
+    fn test_several() {
+        assert_eq!(
+            tokenize("^ab*|d".chars()).unwrap(),
+            vec![Caret, Char('a'), Concat, Char('b'), Star, Pipe, Char('d')]
+        );
+    }
+
+    #[test]
+    fn test_topostfix_without_parens() {
+        // a (+) b* | d*
+        // a b * (+) d |
+        let tokens = tokenize("ab*|d".chars()).unwrap();
+        let postfix = topostfix(tokens).unwrap();
+        assert_eq!(
+            postfix,
+            PostfixTokens(vec![
+                Char('a'),
+                Char('b'),
+                Star,
+                Concat,
+                Char('d'),
+                Pipe,
+            ])
+        );
+
+        // a (+) b * | d * (+) x +
+        // a b * (+) d * x + (+) |
+        let tokens = tokenize("ab*|d*x+".chars()).unwrap();
+        let postfix = topostfix(tokens).unwrap();
+
+        assert_eq!(
+            postfix,
+            PostfixTokens(vec![
+                Char('a'),
+                Char('b'),
+                Star,
+                Concat,
+                Char('d'),
+                Star,
+                Char('x'),
+                Plus,
+                Concat,
+                Pipe
+            ])
+        );
+    }
+
+    #[test]
+    fn test_topostfix_with_parens() {
+        // a (+) (b* | c)
+        // a b * c | (+)
+        let tokens = tokenize("a(b*|c)".chars()).unwrap();
+        let postfix = topostfix(tokens).unwrap();
+
+        assert_eq!(
+            postfix,
+            PostfixTokens(vec![
+                Char('a'),
+                Char('b'),
+                Star,
+                Char('c'),
+                Pipe,
+                Concat,
+            ])
+        );
+    }
+
+    #[test]
+    fn test_topostfix_bad_parens() {
+        let parens = tokenize("((".chars()).unwrap();
+        let postfix = topostfix(parens);
+
+        assert_eq!(postfix, Err(LexError::UnbalancedParens));
+    }
+}
